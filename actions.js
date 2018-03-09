880175a1128cde88d51b345aa0893cf483dbedb4
Piotr Sociński <psocinski@gmail.com>
2018-03-09 13:04:24 +0100

    Finished task

---
+++ actions.js
@@ -0,0 +1,44 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+	return {
+		type: ADD_COMMENT,
+		text,
+		id: uuid.v4()
+	}
+}
+
+function editComment(text, id) {
+	return {
+		type: EDIT_COMMENT,
+		text: text,
+		id: id // id komentarza, który chcemy edytować
+	}
+}
+
+function removeComment(id) {
+	return {
+		type: REMOVE_COMMENT,
+		id: id // id komentarza, który chcemy usunąć
+	}
+}
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id
+	}
+}
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id
+	}
+}
